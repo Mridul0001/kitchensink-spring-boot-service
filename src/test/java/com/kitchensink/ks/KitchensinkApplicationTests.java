37866d14bcdb8a704ae68a5cb29843d2a838e412
Mridul Mahajan <mridulmahajan0001@gmail.com>
2024-09-21 13:19:45 +0530

    Initial commit

---
+++ src/test/java/com/kitchensink/ks/KitchensinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchensink.ks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
